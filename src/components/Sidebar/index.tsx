bd244bf9244decdd3486a777ff67fa24ac522a64
Icaro Oliveira <icarovinici@gmail.com>
2021-05-28 11:36:10 -0300

    refactor: edit sidebar component

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -12,15 +12,20 @@ export default function Sidebar() {
     <Box as='aside' width='64' mr='8'>
       <Stack spacing='12' align='flex-start'>
         <Box>
-          <Text fontSize='small' fontWeight='bold' color='gray.400'>
-            GERAL
+          <Text
+            fontSize='small'
+            fontWeight='bold'
+            color='gray.400'
+            casing='uppercase'
+          >
+            geral
           </Text>
           <Stack spacing='4' mt='8' align='stretch'>
             <Link href='/dashboard'>
               <ChakraLink display='flex' alignItems='center'>
                 <Icon as={RiDashboardLine} fontSize='20' />
-                <Text fontWeight='medium' ml='4'>
-                  Dashboard
+                <Text fontWeight='medium' ml='4' casing='capitalize'>
+                  dashboard
                 </Text>
               </ChakraLink>
             </Link>
@@ -28,8 +33,8 @@ export default function Sidebar() {
             <Link href='/users'>
               <ChakraLink display='flex' alignItems='center'>
                 <Icon as={RiContactsLine} fontSize='20' />
-                <Text fontWeight='medium' ml='4'>
-                  Usuários
+                <Text fontWeight='medium' ml='4' casing='capitalize'>
+                  usuários
                 </Text>
               </ChakraLink>
             </Link>
@@ -37,8 +42,13 @@ export default function Sidebar() {
         </Box>
 
         <Box>
-          <Text fontSize='small' fontWeight='bold' color='gray.400'>
-            AUTOMAÇÃO
+          <Text
+            fontSize='small'
+            fontWeight='bold'
+            color='gray.400'
+            casing='uppercase'
+          >
+            automação
           </Text>
           <Stack spacing='4' mt='8' align='stretch'>
             <ChakraLink display='flex' alignItems='center'>
