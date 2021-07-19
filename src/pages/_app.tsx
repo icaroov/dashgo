470918840ad7bf5c8a78592559a5048e89540bca
Icaro Oliveira <icarovinici@gmail.com>
2021-07-19 20:10:27 -0300

    feat: add miragejs and setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,11 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme'
 import { Layout } from '../components'
 import { SidebarDrawerProvider } from '../hooks/SidebarDrawer'
+import { makeServer } from '../lib/mirage'
+
+if (process.env.NODE_ENV === 'development') {
+  makeServer()
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
