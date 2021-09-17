749393936fef20439e27350ef0f1fb6646632eb7
Icaro Oliveira <icarovinici@gmail.com>
2021-09-17 15:07:12 -0300

    feat: add hook to validate user permissions and roles

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -2,6 +2,8 @@ import { Box, SimpleGrid, Text } from '@chakra-ui/react'
 import dynamic from 'next/dynamic'
 import Head from 'next/head'
 import { useEffect } from 'react'
+import { useAuth } from '../hooks/useAuth'
+import { useCan } from '../hooks/useCan'
 
 import { options, series } from '../lib/apexCharts'
 import { api, setupAPIClient } from '../services/api'
@@ -10,6 +12,12 @@ import { withSSRAuth } from '../utils/withSSRAuth'
 const Chart = dynamic(() => import('react-apexcharts'), { ssr: false })
 
 export default function Dashboard() {
+  const { user } = useAuth()
+
+  const userCanSeeMetrics = useCan({
+    permissions: ['metrics.list'],
+  })
+
   useEffect(() => {
     api
       .get('/me')
@@ -23,21 +31,23 @@ export default function Dashboard() {
         <title>Dashboard | dashgo.</title>
       </Head>
 
-      <SimpleGrid flex='1' gap='4' minChildWidth='320px'>
-        <Box p={['4', '8']} bg='gray.800' borderRadius={8} pb='4'>
-          <Text fontSize='lg' mb='4'>
-            Inscritos da semana
-          </Text>
-          <Chart options={options} series={series} type='area' height={250} />
-        </Box>
+      {userCanSeeMetrics && (
+        <SimpleGrid flex='1' gap='4' minChildWidth='320px'>
+          <Box p={['4', '8']} bg='gray.800' borderRadius={8} pb='4'>
+            <Text fontSize='lg' mb='4'>
+              Inscritos da semana
+            </Text>
+            <Chart options={options} series={series} type='area' height={250} />
+          </Box>
 
-        <Box p={['6', '8']} bg='gray.800' borderRadius={8} pb='4'>
-          <Text fontSize='lg' mb='4'>
-            Taxa de abertura
-          </Text>
-          <Chart options={options} series={series} type='area' height={250} />
-        </Box>
-      </SimpleGrid>
+          <Box p={['6', '8']} bg='gray.800' borderRadius={8} pb='4'>
+            <Text fontSize='lg' mb='4'>
+              Taxa de abertura
+            </Text>
+            <Chart options={options} series={series} type='area' height={250} />
+          </Box>
+        </SimpleGrid>
+      )}
     </>
   )
 }
